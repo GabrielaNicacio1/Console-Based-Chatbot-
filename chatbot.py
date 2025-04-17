35d7287254318daea7515ed3efbe32dd84354da0
Gabriela Nicacio <gabriela.nicacio@wsu.edu>
2025-04-17 09:16:15 -0700

    strip and split chat response to only include answer

--- chatbot.py
+++ chatbot.py
@@ -25,18 +25,19 @@ while True:
   if (user_input.lower() == "exit"): #keep chatting until user says exit
     break
   #add their response to array of history
-  recent_exchanges.append("You: {user_input}")
+  recent_exchanges.append(f"You: {user_input}")
   recent_exchanges = recent_exchanges[-3:] #keep most recent 3
   chat_prompt = "\n".join(recent_exchanges) + "\nChatbot:"
-  #storing in array will look like this 
+  #storing in array should look like this 
   #{You: Hello, Chatbot: Hi I'm a chatbot!, You: Bye}
 
-  response = generator(chat_prompt, max_length = 80, temperature = 0.2)[0]['generated_text'] #set higher max length ??
-  answer = response[len(chat_prompt)]
+  #chat prompt has recent exchanges and prompt to help generate response all in one string tho
+  response = generator(chat_prompt, max_length = max_length, temperature = temperature[0]['generated_text']) #set higher max length ??
+  #response has recent exchanges+ its response so need to strip the first part for just the answer
+  answer = response[len(chat_prompt):].strip().split("\n")[0]
   
   print("Chatbot: ", answer)
-  recent_exchanges.append("Chatbot: {answer}")
+  recent_exchanges.append(f"Chatbot: {answer}")
 
   #else:
    # print("Chatbot: I'm sorry, I don't understand that.")
-  
